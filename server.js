dcba7df4c5dd03d5ec5b97d93f222fec29834752
Natan <natan.redeemed.coder@gmail.com>
2026-08-07 02:41:26 +0000

    Saved progress.

--- server.js
+++ server.js
@@ -1,55 +1,46 @@
-'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const app         = express();
-fccTesting(app);
+"use strict";
+
+
+const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
+const express = require("express");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+
+
 const saltRounds = 12;
-const myPlaintextPassword = 'sUperpassw0rd!';
-const someOtherPlaintextPassword = 'pass123';
+const myPlaintextPassword = "sUperpassw0rd!";
+const someOtherPlaintextPassword = "pass123";
+
+
+const app = express();
+
+fccTesting(app);
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        console.log(res);
+    });
+});
 
 //END_ASYNC
 
+
 //START_SYNC
 
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 //END_SYNC
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log("Listening on port:", PORT)
 });
